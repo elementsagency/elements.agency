2948433afa9bfd2efccdfe92615919c51ec79361
elementsagency <167353790+elementsagency@users.noreply.github.com>
2024-10-21 19:10:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Add 'visible' class to the container once the page is fully loaded
+    const container = document.querySelector('.container');
+    container.classList.add('visible');
+});
